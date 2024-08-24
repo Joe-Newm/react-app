7964d14e052d512d8672261b1d3cb841d1d524c1
unknown <j.newm500@gmail.com>
2024-08-24 00:20:05 -0500

    add index.js and express

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+// test data
+const users = [
+    {id:1, name: "john doe", age: 30},
+    {id:2, name: "jane doe", age: 25}
+];
+
+// API route to get users
+app.get('/api/users', (req, res) => {
+    res.json(users);
+});
+
+app.listen(5000, () => {
+    console.log("server running on port 5000")
+});
