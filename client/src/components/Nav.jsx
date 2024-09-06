f09b17c6eef6ca552b9ae910350458e17808dd9c
unknown <j.newm500@gmail.com>
2024-09-06 15:31:00 -0500

    update sqlite3

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,6 +3,7 @@ function Nav() {
     <nav className="h-14 bg-[#1C1E28] border-b border-b-black flex items-center justify-center px-5 justify-between">
         <button className="border p-2">New</button>
         <h1> Hi im joseph </h1>
+      <h1> hello there </h1>
         <h1 className="text-xl">Backend Notes App Test</h1>
     </nav>
   )
