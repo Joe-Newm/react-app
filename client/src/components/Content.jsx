fde127a5f7b3fa90d7306aeca5f001334c79a9df
Joe-Newm <j.newm500@gmail.com>
2024-09-11 16:55:01 -0500

    test

--- client/src/components/Content.jsx
+++ client/src/components/Content.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 
-function Content({note}) {
+function Content({ note }) {
+  const handleSubmit = (event) => {
+    fetch('http://localhost:8080/send-data', { method: 'POST' })
+  }
+
   return (
     <div className="p-10">
-        {note ? (
+      {note ? (
         <>
           <h1 className="text-3xl mb-5 font-bold">{note.name}</h1>
           <p>{note.content}</p>
         </>
       ) : (
-          <form className="flex">
-            <textarea className="text-3xl mb-5 font-bold" placeholder="Title here..."></textarea>
-            <textarea placeholder="content here..."></textarea>
-          </form>
+        <form className="flex flex-col">
+          <textarea className="text-3xl mb-5 font-bold" placeholder="Title here..."></textarea>
+          <textarea placeholder="content here..."></textarea>
+          <button>Submit</button>
+        </form>
+
       )}
     </div>
   )
--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -1,13 +1,13 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
-export default function UserCard({noteName, onClick, isSelected}) {
-// const [activeNote, setActiveNote] = useState(null);
+export default function UserCard({ noteName, onClick, isSelected }) {
+  // const [activeNote, setActiveNote] = useState(null);
 
 
 
   return (
-    <a className={`cursor-pointer border-b border-b-[#55697E] w-full h-20 flex justify-center items-center hover:bg-slate-800 ${isSelected ? "bg-slate-700" : null }`} onClick={onClick}>
-        <h1>{noteName}</h1>
+    <a className={`cursor-pointer border-b border-b-[#55697E] w-full h-20 flex justify-center items-center hover:bg-slate-800 ${isSelected ? "bg-slate-700" : null}`} onClick={onClick}>
+      <h1>{noteName}</h1>
     </a>
   )
 }  
