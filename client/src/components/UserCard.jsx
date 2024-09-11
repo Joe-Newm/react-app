5916e7b1414be3e7f7fe9431905aaf6d611e7c27
Joe-Newm <j.newm500@gmail.com>
2024-09-11 17:08:58 -0500

    test

--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -2,12 +2,12 @@ import React, { useState } from 'react'
 
 export default function UserCard({ noteName, onClick, isSelected }) {
   // const [activeNote, setActiveNote] = useState(null);
-
+  // hello
 
 
   return (
     <a className={`cursor-pointer border-b border-b-[#55697E] w-full h-20 flex justify-center items-center hover:bg-slate-800 ${isSelected ? "bg-slate-700" : null}`} onClick={onClick}>
       <h1>{noteName}</h1>
-    </a>
+    </a>    
   )
 }  
