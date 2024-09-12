3e125336ae139f3d2dab55902bee02d8047493cf
joe-newm <j.newm500@gmail.com>
2024-09-12 17:09:51 -0500

    change child a tags to buttons inusercard

--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -8,7 +8,7 @@ export default function UserCard({ noteName, onClick, isSelected, onDel }) {
   return (
     <a className={`cursor-pointer border-b border-b-[#55697E] w-full h-20 flex justify-between p-4 items-center hover:bg-slate-800 ${isSelected ? "bg-slate-700" : null}`} onClick={onClick}>
       <h1>{noteName}</h1>
-      <a onClick={onDel} className="rounded-md border p-2">del</a>
+      <button onClick={onDel} className="rounded-md border p-2">del</button>
     </a>
   )
 }  
