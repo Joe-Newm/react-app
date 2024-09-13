e68fca471a05600f28c79394ed728f1cbfdf5a00
Joe-Newm <j.newm500@gmail.com>
2024-09-13 13:41:41 -0500

    fix for deletion not working

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,7 +1,7 @@
 function Nav({ onNewClick }) {
   return (
     <nav className="h-14 bg-[#1C1E28] border-b border-b-black flex items-center justify-center px-5 justify-between">
-      <button onClick={() => onNewClick()} className="bg-slate-700 p-2 rounded-md">New</button>
+      <button onClick={() => onNewClick()} className="bg-slate-700 p-2 rounded-md hover:bg-slate-600">New</button>
       <h1 className="text-xl">Backend Notes App Test</h1>
     </nav>
   )
--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -8,7 +8,7 @@ export default function UserCard({ noteName, onClick, isSelected, onDel }) {
   return (
     <div className={`cursor-pointer border-b border-b-[#55697E] w-full h-20 flex justify-between p-4 items-center hover:bg-slate-800 ${isSelected ? "bg-slate-800" : null}`} onClick={onClick}>
       <h1 className="break-words flex-wrap">{noteName.length > 13 ? noteName.slice(0, 13) + "..." : noteName}</h1>
-      <button onClick={(e) => { e.stopPropagation; onDel; }} className="rounded-md bg-slate-700 p-2 hover:bg-slate-500">del</button>
+      <button onClick={onDel} className="rounded-md bg-slate-700 p-2 hover:bg-slate-600">del</button>
     </div >
   )
 }  
